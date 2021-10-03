1588b0cd6f782b27eaf68a38ef15f5df2c058062
Darkknight131714 <darkknight131714@gmail.com>
2021-10-04 03:00:00 +0530

    New

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'question.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -134,3 +133,18 @@ Widget question(int index){
     ),
   );
 }
+class Question{
+  String name,email,question,description,image,upvote,chat;
+  Question({required this.name,required this.email, required this.description, required this.question,required this.image,required this.chat,required this.upvote});
+}
+
+List<Question> list=[
+  Question(name: "Gautam", email: "darkknight131714@gmail.com", description: "My Question is Practice", question: "I want to know how this works??????????????????????????",image: "images/img.png",chat: "3",upvote: "2"),
+  Question(name: "Gautam", email: "darkknight131714@gmail.com", description: "My Question is Practice", question: "I want to know how this works?",image: "images/img.png",chat: "3",upvote: "1"),
+  Question(name: "Gautam", email: "darkknight131714@gmail.com", description: "My Question is Practice", question: "I want to know how this works?",image: "images/img.png",chat: "3",upvote: "1"),
+  Question(name: "Gautam", email: "darkknight131714@gmail.com", description: "My Question is Practice", question: "I want to know how this works?",image: "images/img.png",chat: "3",upvote: "1"),
+  Question(name: "Gautam", email: "darkknight131714@gmail.com", description: "My Question is Practice", question: "I want to know how this works?",image: "images/img.png",chat: "3",upvote: "1"),
+  Question(name: "Gautam", email: "darkknight131714@gmail.com", description: "My Question is Practice", question: "I want to know how this works?",image: "images/img.png",chat: "3",upvote: "1"),
+  Question(name: "Gautam", email: "darkknight131714@gmail.com", description: "My Question is Practice", question: "I want to know how this works?",image: "images/img.png",chat: "3",upvote: "1"),
+  Question(name: "Gautam", email: "darkknight131714@gmail.com", description: "My Question is Practice", question: "I want to know how this works?",image: "images/img.png",chat: "3",upvote: "1"),
+];
