26f22121168406768b5772e117db19f4a2e50d54
MANSI GAUTAM <62936067+mansigautam777@users.noreply.github.com>
2021-10-12 10:39:52 +0530

    Adding buttons to profile screen

--- lib/home.dart
+++ lib/home.dart
@@ -7,6 +7,20 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  bool bookmarkUpdate(outline) {
+    setState(() {
+      outline = !outline;
+    });
+    return outline;
+  }
+
+  int upvoteIncrement(vote) {
+    setState(() {
+      vote++;
+    });
+    return vote;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -63,98 +77,115 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
-}
 
-Widget question(int index) {
-  return Container(
-    margin: EdgeInsets.all(10),
-    decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(10)),
-        border: Border.all(color: Colors.black, width: 0.3)),
-    child: Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              //SizedBox(width: 50,),
-              Flexible(
-                child: Text(
-                  list[index].question,
-                  softWrap: false,
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 3,
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                ),
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
+  Widget question(int index) {
+    return Container(
+      margin: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          border: Border.all(color: Colors.black, width: 0.3)),
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                CircleAvatar(
-                  child: Image.asset(list[index].image),
+                //SizedBox(width: 50,),
+                Flexible(
+                  child: Text(
+                    list[index].question,
+                    softWrap: false,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 3,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
                 ),
-                SizedBox(
-                  width: 10,
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      list[index].name,
-                      style: TextStyle(fontWeight: FontWeight.w700),
-                    ),
-                    Text(list[index].email),
-                  ],
-                )
               ],
             ),
-          ),
-          Row(children: [
-            Flexible(
-                child: Text(
-              list[index].description,
-              softWrap: false,
-              overflow: TextOverflow.ellipsis,
-              maxLines: 3,
-            ))
-          ]),
-          SizedBox(
-            height: 10,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Icon(CupertinoIcons.up_arrow),
-                    Text(list[index].upvote),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Icon(CupertinoIcons.chat_bubble_fill),
-                    Text(list[index].chat),
-                  ],
-                ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    child: Image.asset(list[index].image),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        list[index].name,
+                        style: TextStyle(fontWeight: FontWeight.w700),
+                      ),
+                      Text(list[index].email),
+                    ],
+                  )
+                ],
               ),
-              Icon(CupertinoIcons.bookmark_fill)
-            ],
-          ),
-        ],
+            ),
+            Row(children: [
+              Flexible(
+                  child: Text(
+                list[index].description,
+                softWrap: false,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 3,
+              ))
+            ]),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      IconButton(
+                          icon: Icon(Icons.arrow_upward_outlined),
+                          onPressed: () {
+                            list[index].upvote = upvoteIncrement(list[index].upvote);
+                          }),
+                      Text("${list[index].upvote}"),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      IconButton(
+                          icon: Icon(Icons.chat_bubble_rounded),
+                          onPressed: () {
+                            
+                          }),
+                      Text(list[index].chat),
+                    ],
+                  ),
+                ),
+                IconButton(
+                    icon: Icon(list[index].bookmark
+                        ? Icons.bookmark_outline
+                        : Icons.bookmark),
+                    onPressed: () {
+                      list[index].bookmark =
+                          bookmarkUpdate(list[index].bookmark);
+                    }),
+              ],
+            ),
+          ],
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
 
 class Question {
-  String name, email, question, description, image, upvote, chat;
+  String name, email, question, description, image, chat;
+  int upvote;
+  bool bookmark;
   Question(
       {required this.name,
       required this.email,
@@ -162,7 +193,8 @@ class Question {
       required this.question,
       required this.image,
       required this.chat,
-      required this.upvote});
+      required this.upvote,
+      required this.bookmark});
 }
 
 List<Question> list = [
@@ -173,7 +205,8 @@ List<Question> list = [
       question: "I want to know how this works??????????????????????????",
       image: "assets/images/img.png",
       chat: "3",
-      upvote: "2"),
+      upvote: 0,
+      bookmark: false),
   Question(
       name: "Gautam",
       email: "darkknight131714@gmail.com",
@@ -181,7 +214,8 @@ List<Question> list = [
       question: "I want to know how this works?",
       image: "assets/images/img.png",
       chat: "3",
-      upvote: "1"),
+      upvote: 0,
+      bookmark: false),
   Question(
       name: "Gautam",
       email: "darkknight131714@gmail.com",
@@ -189,7 +223,8 @@ List<Question> list = [
       question: "I want to know how this works?",
       image: "assets/images/img.png",
       chat: "3",
-      upvote: "1"),
+      upvote: 0,
+      bookmark: false),
   Question(
       name: "Gautam",
       email: "darkknight131714@gmail.com",
@@ -197,7 +232,8 @@ List<Question> list = [
       question: "I want to know how this works?",
       image: "assets/images/img.png",
       chat: "3",
-      upvote: "1"),
+      upvote: 0,
+      bookmark: false),
   Question(
       name: "Gautam",
       email: "darkknight131714@gmail.com",
@@ -205,7 +241,8 @@ List<Question> list = [
       question: "I want to know how this works?",
       image: "assets/images/img.png",
       chat: "3",
-      upvote: "1"),
+      upvote: 0,
+      bookmark: false),
   Question(
       name: "Gautam",
       email: "darkknight131714@gmail.com",
@@ -213,7 +250,8 @@ List<Question> list = [
       question: "I want to know how this works?",
       image: "assets/images/img.png",
       chat: "3",
-      upvote: "1"),
+      upvote: 0,
+      bookmark: false),
   Question(
       name: "Gautam",
       email: "darkknight131714@gmail.com",
@@ -221,7 +259,8 @@ List<Question> list = [
       question: "I want to know how this works?",
       image: "assets/images/img.png",
       chat: "3",
-      upvote: "1"),
+      upvote: 0,
+      bookmark: false),
   Question(
       name: "Gautam",
       email: "darkknight131714@gmail.com",
@@ -229,5 +268,6 @@ List<Question> list = [
       question: "I want to know how this works?",
       image: "assets/images/img.png",
       chat: "3",
-      upvote: "1"),
+      upvote: 0,
+      bookmark: false),
 ];
