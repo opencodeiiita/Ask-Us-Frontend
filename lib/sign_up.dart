2480674f4a344d60cbfd233a41ce510344a7a678
jalanprakhar <iit2020200@iiita.ac.in>
2021-10-14 05:48:22 +0530

    Made the sign up screen responsive

--- lib/sign_up.dart
+++ lib/sign_up.dart
@@ -2,19 +2,45 @@ import 'package:ask_us/login.dart';
 import 'package:ask_us/main.dart';
 import 'package:flutter/material.dart';
 
+class SizeConfig {
+  static late MediaQueryData _mediaQueryData;
+  static late double screenWidth;
+  static late double screenHeight;
+  static late double blockSizeHorizontal;
+  static late double blockSizeVertical;
+  static late double _safeAreaHorizontal;
+  static late double _safeAreaVertical;
+  static late double safeBlockHorizontal;
+  static late double safeBlockVertical;
+
+  void init(BuildContext context) {
+    _mediaQueryData = MediaQuery.of(context);
+    screenWidth = _mediaQueryData.size.width;
+    screenHeight = _mediaQueryData.size.height;
+    blockSizeHorizontal = screenWidth / 100;
+    blockSizeVertical = screenHeight / 100;
+    _safeAreaHorizontal =
+        _mediaQueryData.padding.left + _mediaQueryData.padding.right;
+    _safeAreaVertical =
+        _mediaQueryData.padding.top + _mediaQueryData.padding.bottom;
+    safeBlockHorizontal = (screenWidth - _safeAreaHorizontal) / 100;
+    safeBlockVertical = (screenHeight - _safeAreaVertical) / 100;
+  }
+}
+
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({Key? key}) : super(key: key);
   @override
-    _SignUpScreenState createState() => _SignUpScreenState();
+  _SignUpScreenState createState() => _SignUpScreenState();
 }
-class _SignUpScreenState extends State<SignUpScreen>
-   {
+
+class _SignUpScreenState extends State<SignUpScreen> {
   TextEditingController nameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
-
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Color(0xFFD66853),
@@ -23,17 +49,18 @@ class _SignUpScreenState extends State<SignUpScreen>
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Hero(
-                              tag: 'imageHero',
-                              child:Container(
-                             width: 100.00,
-                             height: 100.00,
-                             decoration: new BoxDecoration(
-                             image: new DecorationImage(
-                                 image: ExactAssetImage('assets/images/askUslogo.png'),
-                                 fit: BoxFit.fitHeight,
-                                 ),
-                               shape: BoxShape.circle,
-                             )),),
+              tag: 'imageHero',
+              child: Container(
+                  width: SizeConfig.blockSizeHorizontal * 25,
+                  height: SizeConfig.blockSizeVertical * 25,
+                  decoration: new BoxDecoration(
+                    image: new DecorationImage(
+                      image: ExactAssetImage('assets/images/askUslogo.png'),
+                      fit: BoxFit.fitHeight,
+                    ),
+                    shape: BoxShape.circle,
+                  )),
+            ),
             Text(
               "AskUs",
               textAlign: TextAlign.center,
@@ -44,7 +71,7 @@ class _SignUpScreenState extends State<SignUpScreen>
             ),
             SizedBox(height: size.height * 0.07),
             Container(
-              width: 300,
+              width: SizeConfig.blockSizeHorizontal * 60,
               decoration: BoxDecoration(
                   color: Color(0xFFF3D1CB),
                   borderRadius: BorderRadius.circular(40),
@@ -53,7 +80,7 @@ class _SignUpScreenState extends State<SignUpScreen>
                       color: Color(0xFFD66853),
                       style: BorderStyle.solid)),
               child: Container(
-                height: 40,
+                height: SizeConfig.blockSizeVertical * 5,
                 child: TextField(
                   decoration: InputDecoration(
                       hintText: 'Email',
@@ -65,7 +92,7 @@ class _SignUpScreenState extends State<SignUpScreen>
             ),
             SizedBox(height: size.height * 0.02),
             Container(
-              width: 300,
+              width: SizeConfig.blockSizeHorizontal * 60,
               decoration: BoxDecoration(
                   color: Color(0xFFF3D1CB),
                   borderRadius: BorderRadius.circular(40),
@@ -74,7 +101,7 @@ class _SignUpScreenState extends State<SignUpScreen>
                       color: Color(0xFFD66853),
                       style: BorderStyle.solid)),
               child: Container(
-                height: 40,
+                height: SizeConfig.blockSizeVertical * 5,
                 child: TextField(
                   decoration: InputDecoration(
                       hintText: 'Password',
@@ -86,7 +113,7 @@ class _SignUpScreenState extends State<SignUpScreen>
             ),
             SizedBox(height: size.height * 0.02),
             Container(
-              width: 300,
+              width: SizeConfig.blockSizeHorizontal * 60,
               decoration: BoxDecoration(
                   color: Color(0xFFF3D1CB),
                   borderRadius: BorderRadius.circular(40),
@@ -95,7 +122,7 @@ class _SignUpScreenState extends State<SignUpScreen>
                       color: Color(0xFFD66853),
                       style: BorderStyle.solid)),
               child: Container(
-                height: 40,
+                height: SizeConfig.blockSizeVertical * 5,
                 child: TextField(
                   decoration: InputDecoration(
                       hintText: 'Confirm Password',
@@ -107,14 +134,14 @@ class _SignUpScreenState extends State<SignUpScreen>
             ),
             SizedBox(height: size.height * 0.07),
             GestureDetector(
-              onTap: (){
-                  Navigator.push(
-                                            context,
-                                            MaterialPageRoute(
-                                                builder: (BuildContext context) => HomePage()));
-                   },
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (BuildContext context) => HomePage()));
+              },
               child: Container(
-                width: 300,
+                width: SizeConfig.blockSizeHorizontal * 60,
                 decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(20),
                     color: Color(0xFFFFFFFF)),
@@ -152,13 +179,13 @@ class _SignUpScreenState extends State<SignUpScreen>
                       //functionality yet to be added
                       ) {
                     Navigator.push(
-                                                                context,
-                                                                MaterialPageRoute(
-                                                                    builder: (BuildContext context) => HomePage()));
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) => HomePage()));
                   },
                   child: Container(
-                    height: 40,
-                    width: 155,
+                    height: SizeConfig.blockSizeVertical * 5,
+                    width: SizeConfig.blockSizeHorizontal * 38,
                     // margin: EdgeInsets.symmetric(horizontal: 10),
                     // padding: EdgeInsets.all(20),
                     decoration: BoxDecoration(
@@ -174,15 +201,15 @@ class _SignUpScreenState extends State<SignUpScreen>
                   ),
                 ),
                 GestureDetector(
-                  onTap: (){
-                      Navigator.push(
-                                                                  context,
-                                                                  MaterialPageRoute(
-                                                                      builder: (BuildContext context) => HomePage()));
-  },
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) => HomePage()));
+                  },
                   child: Container(
-                    height: 40,
-                    width: 155,
+                    height: SizeConfig.blockSizeVertical * 5,
+                    width: SizeConfig.blockSizeHorizontal * 38,
                     // margin: EdgeInsets.symmetric(horizontal: 10),
                     // padding: EdgeInsets.all(20),
                     decoration: BoxDecoration(
@@ -211,9 +238,9 @@ class _SignUpScreenState extends State<SignUpScreen>
                 GestureDetector(
                   onTap: () {
                     Navigator.push(
-                                                                context,
-                                                                MaterialPageRoute(
-                                                                    builder: (BuildContext context) => LoginScreen()));
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) => LoginScreen()));
                   },
                   child: Text(
                     "Log In",
